096a8a89eeb6dd4fb3e5c6a4c3a9447df8c4e9b0
Raad05 <ashabulraad@gmail.com>
2025-03-16 00:06:36 +0600

    completed chapter 02 (controllers)

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /* 
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+
+  // GET /users
+  @Get()
+  findAll(@Query('role') role?: 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  // GET /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return id;
+  }
+
+  // POST /users
+  @Post()
+  createOne(@Body() user: {}) {
+    return user;
+  }
+
+  // PATCH /users/:id
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  // DELETE /users/:id
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {
+    return id;
+  }
+}

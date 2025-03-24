8769c4c08f87c265daba9e4500ab255e100236e5
Luci4 <yamin.raad6109@gmail.com>
2025-03-25 00:17:51 +0600

    completed providers

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -8,9 +8,13 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  // constructor
+  constructor(private readonly usersService: UsersService) {}
+
   /* 
     GET /users
     GET /users/:id
@@ -22,30 +26,30 @@ export class UsersController {
   // GET /users
   @Get()
   findAll(@Query('role') role?: 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   // GET /users/:id
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return id;
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
   }
 
   // POST /users
   @Post()
-  createOne(@Body() user: {}) {
-    return user;
+  createOne(@Body() user: { role: 'ENGINEER' | 'ADMIN' }) {
+    return this.usersService.createOne(user);
   }
 
   // PATCH /users/:id
   @Patch(':id')
-  updateOne(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  updateOne(@Param('id') id: number, @Body() userUpdate: {}) {
+    return this.usersService.updateOne(id, userUpdate);
   }
 
   // DELETE /users/:id
   @Delete(':id')
-  deleteOne(@Param('id') id: string) {
-    return id;
+  deleteOne(@Param('id') id: number) {
+    return this.usersService.deleteOne(id);
   }
 }
--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,4 +1,70 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // properties
+  private users = [
+    {
+      id: 1,
+      role: 'ENGINEER',
+    },
+    {
+      id: 2,
+      role: 'ADMIN',
+    },
+    {
+      id: 3,
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      role: 'ADMIN',
+    },
+    {
+      id: 5,
+      role: 'ENGINEER',
+    },
+  ];
+
+  // findAll
+  findAll(role?: 'ENGINEER' | 'ADMIN') {
+    if (role) return this.users.filter((user) => user.role === role);
+
+    return this.users;
+  }
+
+  // findOne
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  // createOne
+  createOne(user: { role: 'ENGINEER' | 'ADMIN' }) {
+    const newUser = {
+      id: this.users.length,
+      ...user,
+    };
+    this.users.push(newUser);
+
+    return newUser;
+  }
+
+  // updateOne
+  updateOne(id: number, userUpdate: { role?: 'ENGINEER' | 'ADMIN' }) {
+    const user = this.findOne(id);
+    if (user) return { ...user, ...userUpdate };
+
+    return null;
+  }
+
+  // deleteOne
+  deleteOne(id: number) {
+    const user = this.findOne(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return id;
+  }
+}
